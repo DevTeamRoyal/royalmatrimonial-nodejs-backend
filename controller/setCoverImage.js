107d9a2cae8e732f3334f7947213e217dcdeb34a
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2023-04-16 19:47:38 +0530

    changes done for image cover gallery sp

--- controller/setCoverImage.js
+++ controller/setCoverImage.js
@@ -1,17 +1,10 @@
 const { executeStoredProcedure } = require("../helpers/storedProcedure");
 const setUserCoverImage = async (req, res) => {
-  let values = [];
-  if (req.body.galleryImages != null && req.body.coverPhoto != null) {
-    const galleryImages = req.body.galleryImages.length;
-    const coverPhoto = req.body.coverPhoto.length;
-    values = [req.body.userId, galleryImages, coverPhoto];
-  } else {
-    const coverPhotoURI = req.files.map(
-      (file) => `uploads/${req.body.userId}/${file.filename}`
-    );
-    values = [req.body.userId, null, coverPhotoURI[0]];
-  }
-  executeStoredProcedure("sp_set_userImages", [values]).then((result) => {
+  const coverPhotoURI = req.files.map(
+    (file) => `uploads/${req.body.userId}/${file.filename}`
+  );
+  const values = [req.body.userId, coverPhotoURI[0]];
+  executeStoredProcedure("sp_set_userCoverImage", [values]).then((result) => {
     if (result["0"]["output"] < 0) {
       res.json(result);
     } else {
--- controller/setGalleryImages.js
+++ controller/setGalleryImages.js
@@ -1,17 +1,10 @@
 const { executeStoredProcedure } = require("../helpers/storedProcedure");
 const setUserGallery = async (req, res) => {
-  let values = [];
-  if (req.body.galleryImages != null && req.body.coverPhoto != null) {
-    const galleryImages = req.body.galleryImages.length;
-    const coverPhoto = req.body.coverPhoto.length;
-    values = [req.body.userId, galleryImages, coverPhoto];
-  } else {
-    const galleryImagesURIs = req.files.map(
-      (file) => `uploads/${req.body.userId}/${file.filename}`
-    );
-    values = [req.body.userId, JSON.stringify(galleryImagesURIs), null];
-  }
-  executeStoredProcedure("sp_set_userImages", [values]).then((result) => {
+  const galleryImagesURIs = req.files.map(
+    (file) => `uploads/${req.body.userId}/${file.filename}`
+  );
+  const values = [req.body.userId, JSON.stringify(galleryImagesURIs)];
+  executeStoredProcedure("sp_set_userGalleryImages", [values]).then((result) => {
     if (result["0"]["output"] < 0) {
       res.json(result);
     } else {
