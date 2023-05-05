c48f16f3055591191b707898371830ffb900f276
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2023-05-05 11:54:11 +0530

    added multiple gallery images functionality

--- controller/setGalleryImages.js
+++ controller/setGalleryImages.js
@@ -1,24 +1,35 @@
 const { executeStoredProcedure } = require("../helpers/storedProcedure");
 const setUserGallery = async (req, res) => {
-  const galleryImagesURIs = req.files.map(
-    (file) => `uploads/${req.body.userId}/${file.filename}`
-  );
+  let galleryImagesURIs = [];
+  const prevImages = JSON.parse(req.body.PrevImg);
+  if (prevImages.length > 0) {
+    galleryImagesURIs = prevImages.map(
+      (img) => `uploads/${req.body.userId}/${img}`
+    );
+  }
+  galleryImagesURIs = [
+    ...galleryImagesURIs,
+    ...req.files.map((file) => `uploads/${req.body.userId}/${file.filename}`),
+  ];
+
   const values = [req.body.userId, JSON.stringify(galleryImagesURIs)];
-  executeStoredProcedure("sp_set_userGalleryImages", [values]).then((result) => {
-    if (result["0"]["output"] < 0) {
-      res.json(result);
-    } else {
-      try {
-        res.json({
-          ...result["0"],
-          jsonResponse: JSON.parse(result["0"].jsonResponse),
-          status: 200,
-        });
-      } catch (error) {
-        res.send(error);
+  executeStoredProcedure("sp_set_userGalleryImages", [values]).then(
+    (result) => {
+      if (result["0"]["output"] < 0) {
+        res.json(result);
+      } else {
+        try {
+          res.json({
+            ...result["0"],
+            jsonResponse: JSON.parse(result["0"].jsonResponse),
+            status: 200,
+          });
+        } catch (error) {
+          res.send(error);
+        }
       }
     }
-  });
+  );
 };
 
 module.exports = setUserGallery;
