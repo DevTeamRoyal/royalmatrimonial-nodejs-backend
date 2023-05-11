90639830ae77557a53e3f84e19c4815192cfc244
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2023-05-11 16:22:32 +0530

    handle exceptions in logger

--- helpers/logger.js
+++ helpers/logger.js
@@ -7,10 +7,15 @@ const logger = createLogger({
   transports: [
     new transports.DailyRotateFile({
       filename: "logs/%DATE%.log",
+      handleExceptions: true,
+      handleRejections: true,
       datePattern: "YYYY-MM-DD",
       zippedArchive: true,
       maxSize: "20m",
       maxFiles: "7d",
+      options: { flags: "a" },
+      flags: "a",
+      async: true,
     }),
   ],
 });
