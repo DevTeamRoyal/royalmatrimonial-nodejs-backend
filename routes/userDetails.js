cb360b03ad8fe00f0d6c478357e4f2aacdcbf28d
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2023-07-27 10:27:18 +0530

    added funtionality for profile visitor

--- routes/userDetails.js
+++ routes/userDetails.js
@@ -1,10 +1,15 @@
 const profileCompletionScore = require("../controller/user/sp_get_profileCompletionScore");
 const getPartnerDetails = require("../controller/user/sp_get_userPartnerProfile");
+const getUserVisitorListAndCard = require("../controller/user/sp_get_userVisitorListAndCard");
+
 const express = require("express"),
 router = express.Router();
 
 router.post("/profileCompletionScore", (req, res) => {
   profileCompletionScore(req, res);
+});
+router.post("/getUserVisitorListAndCard", (req, res) => {
+  getUserVisitorListAndCard(req, res);
 });
 router.post("/getPartnerDetails", (req, res) => {
   getPartnerDetails(req, res);
