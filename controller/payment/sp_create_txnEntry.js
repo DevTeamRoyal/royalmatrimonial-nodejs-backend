e27bc07a08909c2d4fba0ae72ebd14a618891d40
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2023-08-13 11:46:50 +0530

    added update payment trx

---
+++ controller/payment/sp_create_txnEntry.js
@@ -0,0 +1,45 @@
+const { executeStoredProcedure } = require("../../helpers/storedProcedure");
+
+const createTransaction = (req, res) => {
+  const values = [
+    req.body.userId,
+    req.body.txnid,
+    req.body.status,
+    req.body.productinfo,
+    req.body.mihpayid,
+    req.body.mode,
+    req.body.unmappedstatus,
+    req.body.key,
+    req.body.amount,
+    req.body.discount,
+    req.body.netAmountDebit,
+    req.body.addedon,
+    req.body.field9,
+    req.body.paymentSource,
+    req.body.meCode,
+    req.body.PGTYPE,
+    req.body.bankRefNum,
+    req.body.bankcode,
+    req.body.error,
+    req.body.errorMessage,
+  ];
+  executeStoredProcedure("sp_create_txnEntry", [values]).then(
+    (result) => {
+      if (result["0"]["output"] < 0) {
+        res.json(result);
+      } else {
+        try {
+          res.json({
+            ...result["0"],
+            jsonResponse: JSON.parse(result["0"].jsonResponse),
+            status: 200,
+          });
+        } catch (error) {
+          throw error;
+        }
+      }
+    }
+  );
+};
+
+module.exports = createTransaction;
