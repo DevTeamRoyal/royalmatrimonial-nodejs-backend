07cbb4658a3d754d9475ffc06140263ec43fc79c
Sudhanshu Singh <sudhanshu@datacollaborative.com>
2023-09-17 11:35:29 +0530

    added json to response isMobReg

--- controller/auth/sp_get_isMobileRegistered.js
+++ controller/auth/sp_get_isMobileRegistered.js
@@ -1,24 +1,28 @@
 const { executeStoredProcedure } = require("../../helpers/storedProcedure");
 
 const isMobileRegistered = (req, res) => {
-  const values = [
-    req.body.mobileWithIsd,
-  ];
-  executeStoredProcedure("sp_get_isMobileRegistered", [values]).then((result) => {
-    if (result["0"]["output"] < 0) {
-      res.json({ ...result["0"] });
-    } else {
-      try {
+  const values = [req.body.mobileWithIsd];
+  executeStoredProcedure("sp_get_isMobileRegistered", [values]).then(
+    (result) => {
+      if (result["0"]["output"] < 0) {
         res.json({
           ...result["0"],
-          status: 200,
+          status: 403,
           jsonResponse: JSON.parse(result["0"].jsonResponse),
         });
-      } catch (error) {
-        throw error;
+      } else {
+        try {
+          res.json({
+            ...result["0"],
+            status: 200,
+            jsonResponse: JSON.parse(result["0"].jsonResponse),
+          });
+        } catch (error) {
+          throw error;
+        }
       }
     }
-  });
+  );
 };
 
 module.exports = isMobileRegistered;
