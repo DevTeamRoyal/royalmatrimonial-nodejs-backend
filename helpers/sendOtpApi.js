e0e30e664130cfdd2fd2e97a1fb06544af9af6e3
Sudhanshu Singh <sudhanshu@datacollaborative.com>
2023-09-17 16:21:53 +0530

    added new message template

--- helpers/sendOtpApi.js
+++ helpers/sendOtpApi.js
@@ -18,4 +18,21 @@ const sendOtpApi = async (isdMobile, OTP) => {
   }
 };
 
-module.exports = sendOtpApi;
+const sendForgotPwdOtpApi = async (isdMobile, OTP) => {
+  const userId = "royalsms";
+  const password = "rzee7118RZ";
+  const senderId = "royalM";
+  const onlyMobileNo = isdMobile.substring(2);
+  const msg = `OTP to reset password is ${OTP}. royalM`;
+  const entityID = "1701158080483210201";
+  templateId = "1707168253161399892";
+
+  const otpApi = `http://nimbusit.biz/api/SmsApi/SendSingleApi?UserID=${userId}&Password=${password}&SenderID=${senderId}&Phno=${onlyMobileNo}&Msg=${msg}&EntityID=${entityID}&TemplateID=${templateId}`;
+  try {
+    const response = await axios.get(otpApi);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+module.exports = { sendOtpApi, sendForgotPwdOtpApi };
