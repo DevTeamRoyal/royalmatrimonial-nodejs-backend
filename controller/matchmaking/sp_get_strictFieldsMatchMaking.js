00c020184968b186bd17c76204007882904906b4
Sudhanshu Singh <s.sudhanshu@hotmail.com>
2023-09-28 13:28:04 +0530

    vv1 parsed json res strict

--- controller/matchmaking/sp_get_strictFieldsMatchMaking.js
+++ controller/matchmaking/sp_get_strictFieldsMatchMaking.js
@@ -10,18 +10,14 @@ const getStrictMatchMaking = (req, res) => {
   ];
   executeStoredProcedure("sp_get_strictFieldsMatchMaking", [values]).then(
     (result) => {
-      if (result["0"]["output"] < 0) {
-        res.json(result);
-      } else {
-        try {
-          res.json({
-            ...result["0"],
-            jsonResponse: JSON.parse(result["0"].jsonResponse),
-            status: 200,
-          });
-        } catch (error) {
-          throw error;
-        }
+      try {
+        res.json({
+          ...result["0"],
+          jsonResponse: JSON.parse(result["0"].jsonResponse),
+          status: 200,
+        });
+      } catch (error) {
+        throw error;
       }
     }
   );
